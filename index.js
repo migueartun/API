d3c396814349627f9bf2988d2e591a4bf6d80e24
Juan MIguel <migueartun@gmail.com>
2026-06-25 10:50:32 -0500

    Modals detalle, editar, eliminar + method-override

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 app.set('view engine', 'ejs')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
 
 app.get('/', (req,res) => res.redirect('/clientes'))
 app.use('/clientes', require('./router/enrrutamiento.router'))
